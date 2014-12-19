d0be05e425f1d2e33206171b9983c39f8f5627ac
shv38339 <eleetsvalenzuela@gmail.com>
2014-12-19 14:53:16 -0600

    I forgot what these edits were...oops

--- tidyscheduleSHV.R
+++ tidyscheduleSHV.R
@@ -22,10 +22,27 @@ clean.sched7 <- clean.sched6 %>%
                         spread(type, value, convert = TRUE)
 
 clean.sched7$week <- ifelse(clean.sched7$week > 15, 16, clean.sched7$week) # big oops
+clean.sched7$outcome <- ifelse(clean.sched7$outcome == "w", 1, 0)
+
 # need to add week 17, which are the final rankings
+week17.rankings <- data.frame(game.id = 841:865, week = rep(17, 25), outcome = rep(2, 25), 
+                             abbrvs = rep("", 25), conference = rep(" ", 25), name = rep("", 25), 
+                             pts = rep(0, 25), rank = 25:1)
+week17.rankings$abbrvs <- c("NEB", "MICH", "VAND", "NIU", "SJSU", "ORST", "TEX", "BST", "NU", "UTST", 
+                            "OU", "LSU", "LOU", "KSU", "CLEM", "FSU", "UF", "SC", "STAN", "TAMU", "UGA", 
+                            "ND", "OSU", "ORE", "BAMA")
+week17.rankings$conference <- c("Big Ten", "Big Ten", "Southeastern", "Mid-American", "Western Athletic", 
+                                "Pac-12", "Big 12", "Mountain West", "Big Ten", "Western Athletic", 
+                                "Big 12", "Southeastern", "Big East", "Big 12", "Atlantic Coast", 
+                                "Atlantic Coast", "Southeastern", "Southeastern", "Pac-12", "Southeastern", 
+                                "Southeastern", "Independent", "Big Ten", "Pac-12", "Southeastern")
+week17.rankings$name <- c("Nebraska", "Michigan", "Vanderbilt", "Northern Illinois", "San Jose State", 
+                          "Oregon State", "Texas", "Boise State", "Northwestern", "Utah State", "Oklahoma", 
+                          "Louisiana State", "Louisville", "Kansas State", "Clemson", "Florida State", 
+                          "Florida", "South Carolina", "Stanford", "Texas A&M", "Georgia", "Notre Dame", 
+                          "Ohio State", "Oregon", "Alabama")
 
-
-
+clean.sched8 <- tbl_df(rbind(clean.sched7, week17.rankings))
 
 
 
