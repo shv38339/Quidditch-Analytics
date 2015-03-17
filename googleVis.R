145e1c5d14d16c30699eed8cf36604999ec54d10
shv38339 <eleetsvalenzuela@gmail.com>
2015-03-16 20:27:27 -0400

    alterned name of files, deleted files, or moved them within the project

--- googleVis.R
+++
@@ -1,6 +0,0 @@
-#install.packages("googleVis")
-library(googleVis)
-plot(gvisMotionChart(clean.sched8, idvar = "name", timevar = "week", xvar = "rank", yvar = "pts", 
-                     colorvar = "outcome"))
-plot(gvisMotionChart(clean.schedPTDIFF, idvar = "name", timevar = "week", xvar = "rank", yvar = "ptdiff", 
-                     colorvar = "outcome"))
